1e9b7644878a4342b9cedba3052e4a1d31973c7c
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-11-18 07:14:33 +0000

    python

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     first_character = sentence[0]
     if length == 0:
-        return None
+        return (length, None)
     else:
         return (length, first_character)
