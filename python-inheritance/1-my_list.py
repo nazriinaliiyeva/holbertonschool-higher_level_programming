8d0cc8570065c06e2ba5fe511056f344786836e5
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-11-25 07:51:22 +0000

    python

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""doc"""
+
+
 class MyList(list):
     """A list subclass with a method to print a sorted version."""
 
     def print_sorted(self):
-        """Print the list in ascending order without modifying the original list."""
+        """Print the list in ascending order"""
         print(sorted(self))
-
