b4c6ab66a5571f4128b3fc824898ca8a96b36c27
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-11-26 19:24:27 +0000

    python

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A Square class is created"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class square"""
+
+    def __init__(self, size):
+        """init"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """str"""
+        return f"[Square] {self.__width}/{self.__height}"
