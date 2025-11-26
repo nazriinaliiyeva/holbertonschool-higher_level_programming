46e4dcf088ef467009bda1461479c4b79d766180
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-11-26 19:16:01 +0000

    python

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A Square class"""
+
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """class square"""
+
+    def __init__(self, size):
+        """init"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area"""
+        return self.__size * self.__size
