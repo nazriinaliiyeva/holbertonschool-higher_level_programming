346139a89c747c808eba2f27a0c98e072a9bdba6
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-11-28 06:46:52 +0000

    python

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,3 @@ def read_file(filename=""):
     """Reads a UTF-8 text file and prints its contents to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
-
