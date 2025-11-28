999be7c899348e1b3791c8b0003293456a9a8646
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-11-28 08:03:23 +0000

    python

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student class module with optional attribute filtering"""
+
+
+class Student:
+    """Defines a student with first name, last name, and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return a dictionary representation of the Student instance.
+        If attrs is a list of strings, only include these attributes.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        return self.__dict__.copy()
