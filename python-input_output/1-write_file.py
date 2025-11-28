14d2756ea8eb3bf7f24f888013b3d54846503159
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-11-28 06:54:08 +0000

    python

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""writing to a file"""
+
+
+def write_file(filename="", text=""):
+    """writes to a file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
+
+
+
