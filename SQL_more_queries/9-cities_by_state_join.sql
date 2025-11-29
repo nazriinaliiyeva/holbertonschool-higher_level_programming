6dbb506a5882a3efc94f5bbf3990b9f4dcc3dee0
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-11-29 10:06:12 +0000

    sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- join
-SELECT cities.id, cities.name,states.name AS state 
-FROM cities
-JOIN states ON cities.state_id = states_id
+SELECT cities.id,cities.name,states.name 
+FROM cities,states 
+WHERE cities.state_id = states.id 
 ORDER BY cities.id ASC;
