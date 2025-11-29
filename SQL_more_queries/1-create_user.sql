b09d7715dbe82aae091f3e67b33dd618569f95c6
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-11-29 08:46:11 +0000

    sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create user
-CREATE USER IF NOT EXISTS 'user_0d_01'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE user IF NOT EXISTS 'user_0d_01'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_01'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
