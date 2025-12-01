b6d31966a72744c8206677b9e9906c633c68ffec
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-12-01 09:44:23 +0000

    python

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -7,7 +7,6 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
     else:
