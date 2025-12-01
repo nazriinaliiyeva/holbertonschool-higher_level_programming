15eae280abe7c78ed9d7dfcfff03359190a9404a
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-12-01 07:29:08 +0000

    python

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -11,6 +11,8 @@ from urllib import request
 if "__name__" == "__main__":
     url = "https://intranet.hbtn.io/status"
 
+    req = request.Request(url, headers={"cfclearance": "true"})
+
     with request.urlopen(url) as response:
 
         body = response.read()
