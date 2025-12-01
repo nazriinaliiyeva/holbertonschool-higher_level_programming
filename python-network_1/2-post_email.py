6d1f792c76be53e5396f45d3b687456b6881ede5
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-12-01 08:59:53 +0000

    python

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     email = sys.argv[2]
 
     data = parse.urlencode({"email": email}).encode("utf-8")
-    req = request.Request(url, data=data, method="POST"
+    req = request.Request(url, data=data, method="POST")
     with request.urlopen(req) as response:
         body = response.read()
         print(body.decode("utf-8"))
