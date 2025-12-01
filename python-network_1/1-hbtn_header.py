6e03966c2d1fb17b4a32e108c4489b929d3de4bf
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-12-01 08:29:25 +0000

    python

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -9,5 +9,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     with request.urlopen(url) as response:
         x_request_id = response.getheader("X-Request-Id")
-        if x_request_id is not None:
-            print(x_request_id)
+        print(x_request_id)
