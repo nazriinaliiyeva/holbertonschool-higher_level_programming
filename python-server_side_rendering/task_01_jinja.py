2ec8412835ddeeed036072297955ac3c38689522
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-12-06 10:38:27 +0000

    python

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -15,5 +15,5 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, use_reloader=False, port=5000)
 
